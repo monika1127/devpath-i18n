45966c53bb24f4fcfbfb6b98f724b265b8912f26
monika1127 <dziubmonika@gmail.com>
2023-06-18 21:23:50 +0200

    add: axe-core

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n/i18n.ts";
+import reportAccessibility from "./utils/reportAccessibility.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
