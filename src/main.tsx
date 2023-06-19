4a372eadb85abac4d59c05b923e902f67e706a6b
monika1127 <dziubmonika@gmail.com>
2023-06-19 10:08:46 +0200

    add: auto test

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import axe from "@axe-core/react";
 import "./i18n/i18n.ts";
-import reportAccessibility from "./utils/reportAccessibility.ts";
+
+const axeConf = {
+  rules: [{ id: "skip-link", enabled: true }],
+};
+
+if (process.env.NODE_ENV !== "production") {
+  axe(React, ReactDOM, 1000, axeConf);
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
